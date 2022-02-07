818bd63f18e8e5226a119062b6b8af0bc9ce3da4
tellemorales <62278479+tellemorales@users.noreply.github.com>
2022-02-07 19:03:22 +0800

    Initial Navigation Bar

---
+++ indie-game-space/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import './App.css';
+import ReactDOM from "react-dom";
+import NavBar from './components/NavBar';
+
+function App() {
+  return(
+    <div className="App">
+      <NavBar />
+      
+    </div>
+  );
+}
+
+export default App;
