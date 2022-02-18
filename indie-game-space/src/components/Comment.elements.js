c14a910fb7013b7196b2c9329dc46c8b86a4bd0e
Guiaochino <chinotiamzon@gmail.com>
2022-02-18 20:58:42 +0800

    Comment Components Added (NOTE: Unfinished)

---
+++ indie-game-space/src/components/Comment.elements.js
@@ -0,0 +1,2 @@
+// This is where the styled component for the Comment Display and Form will be
+// TODO: Create Styled Components
---
+++ indie-game-space/src/components/CommentComponent.js
@@ -0,0 +1,24 @@
+// This Component is a Comment Template for every comment in the game in the data base
+
+// Import Dependencies
+// Icons
+import { FaRegThumbsUp, FaRegThumbsDown } from "react-icons/fa"
+import {  } from "./Comment.elements"
+
+
+// This is a comment DISPLAY
+const CommentDisplay = (props) => {
+    // return(
+    //     // TODO: Structure of Comment to Display on the webpage
+    // );
+}
+
+
+// This is for comment FORMS
+const CommentForm = () => {
+    // return(
+    //     // TODO: Structure for Forms when Commenting
+    // )
+}
+
+export default { CommentDisplay, CommentForm };
