d3071712f4eadb113eaabb0d3ff1b10b0e961b4b
Guiaochino <chinotiamzon@gmail.com>
2022-03-01 15:53:07 +0800

    API for Back-end added

---
+++ indie-game-space/src/api/database.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+    header("Content-Type: application/json; charset=UTF-8");
+
+    // Properties
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $database = 'gamespace';
+
+    // Establish Connection
+    $connection = mysqli_connect($host, $user, $pass, $database);
+
+    if (!$connection){
+        die('Connection Failed' . mysqli_connect_error());
+    }
+
+?>
