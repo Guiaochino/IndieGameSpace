2dc9bff92269ab171c635aea1b608637523e87ad
Guiaochino <chinotiamzon@gmail.com>
2022-03-02 16:57:35 +0800

    Redirecting from GameList to GameProfile feature

--- indie-game-space/src/pages/Games.js
+++ indie-game-space/src/pages/Games.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 import TopFiller from '../components/TopFiller/TopFiller';
 import GamesContent from './GamesContent';
@@ -8,10 +9,10 @@ const Games = () => {
   return (
     <>  
         <TopFiller />
-        <GamesContent {...gamesObjOne}/>
-        <GamesContent {...gamesObjTwo}/>
-        <GamesContent {...gamesObjThree}/>
-        <GamesContent {...gamesObjFour}/>
+        <Link to="GameProfile"><GamesContent {...gamesObjOne}/></Link>
+        <Link to="GameProfile"><GamesContent {...gamesObjTwo}/></Link>
+        <Link to="GameProfile"><GamesContent {...gamesObjThree}/></Link>
+        <Link to="GameProfile"><GamesContent {...gamesObjFour}/></Link>
         <Footer />
     </>
   );
