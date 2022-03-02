88ffd5391e12bfcd1b6e4f9622a7e8c4e58a19b2
Guiaochino <chinotiamzon@gmail.com>
2022-03-03 06:38:20 +0800

    DevProfile Feature Unfinished Design

--- indie-game-space/src/components/DevProfile/DevProfile.js
+++ indie-game-space/src/components/DevProfile/DevProfile.js
@@ -1,27 +1,50 @@
 
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './DevProfile.css';
+import { GiConsoleController } from 'react-icons/gi';
+import { MdPublish } from 'react-icons/md';
+import { CgProfile } from 'react-icons/cg';
 
 export default class DevProfile extends Component {
+
   render() {
     return (
-      
-      <div>
-
-        <div>
-          <img src='' alt=''/>
-          <p> Developer's Name </p>
-          <p> Developer's Email </p>
-          <div>
-            Members Name / Personal Name
+      <>
+        <div className='account-container'>
+          
+          {/* Profile View */}
+          <div className='profile-container'>
+            <img src='filename.jpg' alt='SomePicture' />
+            <h4> Developer's Name </h4>
+            <div>
+              Members Name / Personal Name
+            </div>
+            <h6> Developer's Email </h6>
+            
+            <button className='btn btn-dark block'> Edit Profile </button>
           </div>
-          <button> Edit Profile </button>
+
+          {/* Games and Post Game View */}
+          <div className='viewing-contianer'>
+
+            <div className='navigation'>
+              <Link className='nav-element'> <GiConsoleController /> Games </Link>
+              <Link className='nav-element'> <MdPublish /> Post </Link>
+              <Link className='nav-element'> <CgProfile /> Profile </Link>
+            </div>
+
+            <div> { this.defaultView } </div>
+            {/* TODO: List of Published Games by Specific Developer */}
+
+            {/* TODO: Form for Publishing Game */}
+
+            {/* Display Profile for Edit */}
+          
+          </div>
+
         </div>
-        
-        <div></div>
-
-      </div>
-
+      </>
     )
   }
 }
