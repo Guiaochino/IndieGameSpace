4b963088b3f6d90cb595bec1b1cc8adc08af1573
Guiaochino <chinotiamzon@gmail.com>
2022-03-04 03:20:22 +0800

    Developer Profile Update Lacking Profile Edit

--- indie-game-space/src/api/updateAccount.php
+++ indie-game-space/src/api/updateAccount.php
@@ -5,10 +5,10 @@
 
     require "./database.php";
 
-    $user = 'guiaochino';
-    $surname = 'tiamzon';
-    $givenname = 'guiaochino';
-    $middlename = 'gonzales';
+    // $user = 'guiaochino';
+    // $surname = 'tiamzon';
+    // $givenname = 'guiaochino';
+    // $middlename = 'gonzales';
 
     $query = "UPDATE dev_profile SET surname = $surname, givenname = $givenname, middlename = $middlename WHERE devName = $user";
     $result = mysqli_query($connection, $query);
